ce943024e73efd8b705401b17ab225c76fd4e2bf
harry-kt <Harsh.Kishorsingh.Thakur@gmail.com>
2022-05-01 20:24:34 -0700

    Detection

--- src/Detector.py
+++ src/Detector.py
@@ -10,7 +10,7 @@ import numpy as np
 
 from tensorflow.python.keras.utils.data_utils import get_file
 
-np.random.seed(53)
+np.random.seed(6)
 
 class Detector:
     def __init__(self) -> None:
@@ -51,9 +51,52 @@ class Detector:
 
         return
 
-    def predict_image(self, image_path):
+    def create_bounding_box(self, image, threshold=0.5):
+        input_tensor = cv2.cvtColor(image.copy(), cv2.COLOR_BGR2RGB)
+        input_tensor = tf.convert_to_tensor(input_tensor, dtype=np.uint8)
+        input_tensor = input_tensor[tf.newaxis, ...]
+
+        detections = self.model(input_tensor)
+
+        bboxes = detections['detection_boxes'][0].numpy()
+        class_indexes = detections['detection_classes'][0].numpy().astype(np.int32)
+        class_scores = detections['detection_scores'][0].numpy()
+
+        img_h, img_w, img_c = image.shape
+
+        bbox_index = tf.image.non_max_suppression(bboxes, class_scores, max_output_size=50, iou_threshold=threshold, score_threshold=0.5)
+
+        if len(bbox_index) != 0:
+            for i in bbox_index:
+                bbox = tuple(bboxes[i].tolist())
+                class_confidence = round(100*class_scores[i])
+                class_index = class_indexes[i]
+
+                class_label_text = self.classesList[class_index]
+                class_color = self.colorList[class_index]
+
+                display_text = '{}: {}%'.format(class_label_text, class_confidence)
+
+                y_min, x_min, y_max, x_max = bbox
+                x_min, x_max, y_min, y_max = (x_min*img_w, x_max*img_w, y_min*img_h, y_max*img_h)
+                x_min, x_max, y_min, y_max =  int(x_min), int(x_max), int(y_min), int(y_max)
+
+                cv2.rectangle(image, (x_min, y_min), (x_max, y_max), color=class_color, thickness=1)
+                cv2.putText(image, display_text, (x_min, y_min-10), cv2.FONT_HERSHEY_PLAIN, 1, class_color, thickness=2)
+
+                line_width = min(int((x_max - x_min)*0.2), int((y_max - y_min)*0.2))
+
+                cv2.line(image, (x_min, y_min), (x_min+line_width, y_min), class_color, thickness=5)
+                cv2.line(image, (x_min, y_min), (x_min, y_min+line_width), class_color, thickness=5)
+
+        return image
+
+    def predict_image(self, image_path, threshold=0.5):
         image = cv2.imread(image_path)
 
-        cv2.imshow("Result", image)
+        bbox_image = self.create_bounding_box(image)
+
+        cv2.imwrite(self.model_name + ".jpg", bbox_image)
+        cv2.imshow("Result", bbox_image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
--- src/main.py
+++ src/main.py
@@ -1,11 +1,16 @@
 import os
+
+from numpy import imag
 from Detector import *
 
 model_url = "http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8.tar.gz"
 label_path = os.path.abspath("../Object-detection-model/data/labels/coco.names")
 image_path = os.path.abspath("../Object-detection-model/data/images/test_1.jpg")
 
+threshold = 0.5
+
 detector = Detector()
 detector.readClasses(label_path)
 detector.download_model(model_url)
 detector.load_model()
+detector.predict_image(image_path, threshold=threshold)
